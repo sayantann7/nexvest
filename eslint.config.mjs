b412a4b8c7cee5df4aed24733633ab530cd1e698
Sayantan Nandi <sayantannandi13@gmail.com>
2025-04-29 15:23:24 +0530

    fixing prisma errors - 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "lib/generated/**/*"
+    ]
+  },
+
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
